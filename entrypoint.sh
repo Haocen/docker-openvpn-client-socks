27164bd60b9461f92292a747b4c94421f0459eb6
Haocen <Haocen@users.noreply.github.com>
2024-07-16 11:26:29 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ cd /etc/openvpn
 while true; do
     echo "Setting sockd port to $PORT"
     sed -i -e "s/^internal: eth0 port.*$/internal: eth0 port = $PORT/g" /etc/sockd.conf
+    sed -i -e "s/^external: tun0$/external: $TUN_DEVICE/g" /etc/sockd.conf
     echo "Starting OpenVPN"
     /usr/sbin/openvpn --data-ciphers AES-256-GCM:AES-128-GCM:AES-256-CBC:AES-128-CBC --config *.ovpn --script-security 2 --up /usr/local/bin/sockd.sh || true
     echo "OpenVPN exited. Trying again in 1 second."
