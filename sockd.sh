b7aef2a60a9ec9d7e2d1a3ceff57f3858f40e275
Haocen Xu <admin@haocen.me>
2025-08-24 01:39:33 -0400

    Fix broken sockd.sh

--- sockd.sh
+++ sockd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 [ -f /etc/openvpn/up.sh ] && /etc/openvpn/up.sh "$@"
-pkill sockd || true # Kill exising sockd if any
+if pgrep sockd > /dev/null; then
+  echo "An existing sockd process was found. Terminating it."
+  pkill sockd
+else
+  echo "No existing sockd process found. Starting a new one."
+fi
 /usr/sbin/sockd -D
